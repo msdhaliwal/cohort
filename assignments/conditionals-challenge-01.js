fd80d67235c58bc422041fa00695dd995b736b2a
Manpreet Singh <msdhaliwal@outlook.in>
2025-03-10 08:01:49 +0530

    assignment: conditionals 1

---
+++ assignments/conditionals-challenge-01.js
@@ -0,0 +1,9 @@
+/*
+You are organizing a party and have a list of invited guests. But just before the party starts, a new friend decides to join.You need to add them to the guest list.
+Problem Statement: Create a function that adds a new guest to the guest list and returns the updated list. */
+
+// You just need to implement the checkEvenOdd function
+function checkEvenOdd(num) {
+	// Return "Even" if num is even, otherwise return "Odd"
+	return num % 2 == 0 ? 'Even' : 'Odd';
+}
